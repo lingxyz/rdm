1f05019dbccaa1db359575ec4b94a0200867c94b
Ling Z <771152260@qq.com>
2022-08-05 18:12:33 +0800

    自定义域名

--- web-vue3/deploy.sh
+++ web-vue3/deploy.sh
@@ -9,11 +9,16 @@ npm run build
 # 进入生成的文件夹
 cd dist
 
+# 如果是发布到自定义域名
+echo 'rdm.zhangling.site' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:lingxyz/rdm.git master:gh-pages
+
+echo 'web published done! visit at: https://zhangling.site/rdm/'
 
 cd -
